4c0e0aa5630a0d28d129916a21fc2a2a515ef60f
ajay birla <ajay.birla@gemsessence.com>
2026-06-13 17:50:42 +0530

    Added todo form

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import styles from './App.module.css'
-import { TodoForm } from './components/TodoForm'
+import { TodoForm } from './components/todoform/TodoForm'
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleCreate = (newTodo) => {
+    setTodos(prevTodos => [...prevTodos, { id: `${prevTodos.length + 1}`, ...newTodo }
+    ]);
+  }
+
   return (
     <div className={styles.App}>
       <header className={styles.Header}>
         <img className={styles.Logo} src="/to-do.png"/>
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
-      <div className={styles.AppContainer}><TodoForm/></div>
+      <div className={styles.AppContainer}>
+        <TodoForm onCreate={handleCreate} />
+        {JSON.stringify(todos)}
+      </div>
     </div>
   )
 }
