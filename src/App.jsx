a88fae0eb5c6e2f78ef375c86921ee567ff006fe
ajay birla <ajay.birla@gemsessence.com>
2026-07-11 18:07:36 +0530

    Display alert for errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { TodoForm } from './components/todoform/TodoForm'
 import { TodoList } from './components/todolist/TodoList';
 import { TodoFilters } from './components/TodoFilters/TodoFilters';
 import { useTodos } from './hooks/useTodos';
+import { Alert } from './alert/Alert';
 
 const App = () => {
   const todos = useTodos();
@@ -14,6 +15,9 @@ const App = () => {
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
       <div className={styles.AppContainer}>
+        {!!todos.error.message && (
+          <Alert onClear={todos.error.clear}>{todos.error.message}</Alert>
+        )}
         <TodoForm onCreate={todos.create} />
         <TodoFilters onFilter={todos.filter} />
         <TodoList todos={todos.data} onUpdate={todos.update} onDelete={todos.delete}/>
--- src/hooks/useTodos.js
+++ src/hooks/useTodos.js
@@ -4,13 +4,14 @@ import { api } from "../api";
 export const useTodos = () => {
   const [todos, setTodos] = useState([]);
   const [filters, setFilters] = useState({});
+  const [errorMessage, setErrorMessage] = useState();
 
   const fetchTodos = async () => {
     try {
       const data = await api.todos.getAll(filters);
       setTodos(data)
     } catch (error) {
-      console.log("Failed to get todo's. Please try again later.");
+      setErrorMessage("Failed to get todo's. Please try again later.");
     }
   }
 
@@ -23,7 +24,7 @@ export const useTodos = () => {
       await api.todos.create(newTodo);
       await fetchTodos();
     } catch (error) {
-      console.log("Failed to create todo. Please try again later.");
+      setErrorMessage("Failed to create todo. Please try again later.");
     }
   }
 
@@ -32,7 +33,7 @@ export const useTodos = () => {
       await api.todos.update(id, newTodo);
       await fetchTodos();
     } catch (error) {
-      console.log("Failed to update todo. Please try again later.");
+      setErrorMessage("Failed to update todo. Please try again later.");
     }
   }
 
@@ -41,7 +42,7 @@ export const useTodos = () => {
       await api.todos.delete(id);
       await fetchTodos();
     } catch (error) {
-      console.log("Failed to delete todo. Please try again later.");
+      setErrorMessage("Failed to delete todo. Please try again later.");
     }
   }
 
@@ -52,5 +53,9 @@ export const useTodos = () => {
     create: handleCreate,
     update: handleUpdate,
     delete: handleDelete,
+    error: {
+      message: errorMessage,
+      clear: () => setErrorMessage(),
+    },
   };
 }
