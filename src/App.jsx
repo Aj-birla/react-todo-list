3a37ff394304c83bf9e93cae68d80f9382d10566
ajay birla <ajay.birla@gemsessence.com>
2026-07-11 17:52:44 +0530

    Created costum hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,11 @@
-import { useEffect, useState } from 'react'
 import styles from './App.module.css'
 import { TodoForm } from './components/todoform/TodoForm'
 import { TodoList } from './components/todolist/TodoList';
 import { TodoFilters } from './components/TodoFilters/TodoFilters';
-import { api } from "./api";
+import { useTodos } from './hooks/useTodos';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [filters, setFilters] = useState({});
-
-  const fetchTodos = async () => {
-    try {
-      const data = await api.todos.getAll(filters);
-      setTodos(data)
-    } catch (error) {
-      console.log("Failed to get todo's. Please try again later.");
-    }
-  }
-
-  useEffect(() => {
-    fetchTodos();
-  }, [filters]);
-
-  const handleCreate = async (newTodo) => {
-    try {
-      await api.todos.create(newTodo);
-      await fetchTodos();
-    } catch (error) {
-      console.log("Failed to create todo. Please try again later.");
-    }
-  }
-
-  const handleUpdate = async(id, newTodo) => {
-    try {
-      await api.todos.update(id, newTodo);
-      await fetchTodos();
-    } catch (error) {
-      console.log("Failed to update todo. Please try again later.");
-    }
-  }
-
-  const handleDelete = async (id) => {
-    try {
-      await api.todos.delete(id);
-      await fetchTodos();
-    } catch (error) {
-      console.log("Failed to delete todo. Please try again later.");
-    }
-  }
+  const todos = useTodos();
 
   return (
     <div className={styles.App}>
@@ -56,9 +14,9 @@ const App = () => {
         <h2 className={styles.Title}>To-Do App</h2>
       </header>
       <div className={styles.AppContainer}>
-        <TodoForm onCreate={handleCreate} />
-        <TodoFilters onFilter={setFilters} />
-        <TodoList todos={todos} onUpdate={handleUpdate} onDelete={handleDelete}/>
+        <TodoForm onCreate={todos.create} />
+        <TodoFilters onFilter={todos.filter} />
+        <TodoList todos={todos.data} onUpdate={todos.update} onDelete={todos.delete}/>
       </div>
     </div>
   )
---
+++ src/hooks/useTodos.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import { api } from "../api";
+
+export const useTodos = () => {
+  const [todos, setTodos] = useState([]);
+  const [filters, setFilters] = useState({});
+
+  const fetchTodos = async () => {
+    try {
+      const data = await api.todos.getAll(filters);
+      setTodos(data)
+    } catch (error) {
+      console.log("Failed to get todo's. Please try again later.");
+    }
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, [filters]);
+
+  const handleCreate = async (newTodo) => {
+    try {
+      await api.todos.create(newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to create todo. Please try again later.");
+    }
+  }
+
+  const handleUpdate = async(id, newTodo) => {
+    try {
+      await api.todos.update(id, newTodo);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to update todo. Please try again later.");
+    }
+  }
+
+  const handleDelete = async (id) => {
+    try {
+      await api.todos.delete(id);
+      await fetchTodos();
+    } catch (error) {
+      console.log("Failed to delete todo. Please try again later.");
+    }
+  }
+
+  return {
+    data: todos,
+    fetch: fetchTodos,
+    filter: setFilters,
+    create: handleCreate,
+    update: handleUpdate,
+    delete: handleDelete,
+  };
+}
